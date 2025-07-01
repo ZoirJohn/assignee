298eb26dae6061a9335280cb49e8d756e28ff704
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-07-02 01:39:41 +0500

    Refactor TeacherTabs component to improve state management and enhance message handling. Update user ID handling and message fetching logic for better performance and clarity. Implement dynamic rendering of messages based on user authentication, streamlining user interactions.

---
+++ lib/supabase/middleware.ts
@@ -0,0 +1,55 @@
+import { createServerClient } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
+
+export async function updateSession(request: NextRequest) {
+        let supabaseResponse = NextResponse.next({
+                request,
+        })
+
+        const supabase = createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+                cookies: {
+                        getAll() {
+                                return request.cookies.getAll()
+                        },
+                        setAll(cookiesToSet) {
+                                cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
+                                supabaseResponse = NextResponse.next({
+                                        request,
+                                })
+                                cookiesToSet.forEach(({ name, value, options }) => supabaseResponse.cookies.set(name, value, options))
+                        },
+                },
+        })
+
+        const {
+                data: { user },
+        } = await supabase.auth.getUser()
+
+        const isNotHome = request.nextUrl.pathname != '/'
+        const isNotSignIn = !request.nextUrl.pathname.startsWith('/signin')
+        const isNotSignUp = !request.nextUrl.pathname.startsWith('/signup')
+        const isNotConfirm = !request.nextUrl.pathname.startsWith('/confirm')
+
+        const routeCheck = isNotSignIn || isNotSignUp || isNotConfirm || isNotHome
+        const isDashboard = request.nextUrl.pathname.startsWith('/dashboard')
+
+        if (!user && isDashboard) {
+                const url = request.nextUrl.clone()
+                url.pathname = '/'
+                return NextResponse.redirect(url)
+        }
+
+        if (user && routeCheck && user.user_metadata.role == 'teacher' && !request.nextUrl.pathname.startsWith('/dashboard/teacher')) {
+                const url = request.nextUrl.clone()
+                url.pathname = '/dashboard/teacher'
+                return NextResponse.redirect(url)
+        }
+
+        if (user && routeCheck && user.user_metadata.role == 'student' && !request.nextUrl.pathname.startsWith('/dashboard/student')) {
+                const url = request.nextUrl.clone()
+                url.pathname = '/dashboard/student'
+                return NextResponse.redirect(url)
+        }
+
+        return supabaseResponse
+}
